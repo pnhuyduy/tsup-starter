9f39cb75ccec6e5c6acf4ab20c84948d167f847b
huyduy <huyduy.work@gmail.com>
2023-12-12 01:13:31 +0700

    first init

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig((options) => {
+  const IS_PROD = !options.watch
+  return {
+    entry: ["src/index.ts"],
+    splitting: false,
+    sourcemap: IS_PROD,
+    clean: true,
+    format: ["cjs"],
+    dts: IS_PROD,
+    minify: IS_PROD,
+    onSuccess: "node dist/index.js",
+  }
+})
