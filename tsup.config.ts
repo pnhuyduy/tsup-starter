ffc9b9c15ee791b33d24eb69504dc210287a6de8
huyduy <huyduy.work@gmail.com>
2023-12-12 01:14:54 +0700

    add esm format

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "tsup"
+import { defineConfig } from 'tsup'
 
 export default defineConfig((options) => {
   const IS_PROD = !options.watch
   return {
-    entry: ["src/index.ts"],
+    entry: ['src/index.ts'],
     splitting: false,
-    sourcemap: IS_PROD,
+    sourcemap: !IS_PROD,
     clean: true,
-    format: ["cjs"],
+    format: ['cjs', 'esm'],
     dts: IS_PROD,
     minify: IS_PROD,
-    onSuccess: "node dist/index.js",
+    onSuccess: 'node dist/index.js',
   }
 })
